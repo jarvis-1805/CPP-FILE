51f8f42b58b735a661d4ad07bbe2b46ad3be3282
jarvis-1805 <shubhsahu1805@gmail.com>
2020-05-08 01:54:55 +0530

    ANOTHER CAHNGE

--- Ques3.cpp
+++ Ques3.cpp
@@ -3,66 +3,146 @@ check if a relation is reflexive, Symmetric, Anti-symmetric and Transitive. Writ
 to use this class.
 */
 #include<iostream>
+#include<stdio.h>
+#include<conio.h>
+
 using namespace std;
+
 class RELATION
 {
 	private:
-	int i,j,nA,nB,nR,*A,*B,*R;
+	int i,j,k,x,y,iA,iB,nA,nR,*A,*R,**RM;
 	
 	public:
-	void inputSets()
-	{
-		cout << "Enter the size of SET A : ";
-		cin >> nA;
-		A = new int[nA];
-		cout << "Enter the elements : ";
-		for(i=0; i<nA; i++)
-			cin >> A[i];
-		cout << "Enter the size of SET B : ";
-		cin >> nB;
-		B = new int[nB];
-		cout << "Enter the elements : ";
-		for(i=0; i<nB; i++)
-			cin >> B[i];
-	}
-	
-	void inputRelation()
-	{
-		cout << "Enter the no of relations : ";
-		cin >> nR;
-		R = new int[nR * 2];
-		cout << "Enter the relations in pair :\n";
-		for(i=0; i<nR*2; i++)
-			cin >> R[i];
-	}
-	
-	void printSets()
-	{
-		cout << "A = {";
-		for(i=0; i<nA; i++)
-			cout << A[i] << " ";
-		cout << "}\n";
-		cout << "B = {";
-		for(i=0; i<nB; i++)
-			cout << B[i] << " ";
-		cout << "}\n";
-	}
-	
-	void printRelation()
-	{
-		cout << "R = {";
-		for(i=0; i<nR*2; i++)
-			cout << R[i] << " ";
-		cout << "}\n";
-	}
+	void inputSet();
+	void inputRelation();
+	void printSet();
+	void printRelation();
+	void Matrix();
+	void reflexive();
 };
+
+void RELATION::inputSet()
+{
+	cout << "Enter the size of SET A : ";
+	cin >> nA;
+	A = new int[nA];
+	cout << "Enter the elements : ";
+	for(i=0; i<nA; i++)
+		cin >> A[i];
+}
+
+void RELATION::inputRelation()
+{
+	cout << "Enter the no of relations (R on A) : ";
+	cin >> nR;
+	R = new int[nR * 2];
+	cout << "Enter the relations in pair :\n";
+	for(i=0; i<nR*2; i++)
+		cin >> R[i];
+}
+
+void RELATION::printSet()
+{
+	cout << "A = {";
+	for(i=0; i<nA; i++)
+		cout << A[i] << " ";
+	cout << "}\n";
+}
+
+void RELATION::printRelation()
+{
+	cout << "R = {";
+	for(i=0; i<nR*2; i++)
+	{
+		if(i%2 == 0)
+			cout << "(";
+		cout << R[i] << " ";
+		if(i%2 != 0)
+			cout << ")";
+	}
+	cout << "}\n";
+}
+
+void RELATION::Matrix()
+{
+	cout << "\nMATRIX NOTATION\n\n";
+	RM = new int *[nA];
+	for(i=0; i<nA; i++)
+		RM[i]=new int[nA];
+	
+	for(i=0; i<nA; i++)
+	{
+		for(j=0; j<nA; j++)
+		{
+			RM[i][j]=0;
+		}	
+	}
+	
+	for(i=0; i<nR*2; i+=2)
+	{
+		for(j=0; j<nA; j++)
+		{
+			if(R[i] == A[j])
+			{
+				iA=j;
+				break;
+			}
+		}
+		for(k=0; k<nA; k++)
+		{
+			if(R[i+1] == A[k])
+			{
+				iB=k;
+				break;
+			}
+		}
+		RM[iA][iB]=1;
+	}
+	
+	cout << "    ";
+	for(int x=0; x<nA; x++)
+		cout << " " << A[x] << " ";
+	cout << endl << endl;
+	for(i=0; i<nA; i++)
+	{
+		cout << A[i] << "  | ";
+		for(j=0; j<nA; j++)
+		{
+			cout << RM[i][j] << "  ";
+		}	
+		cout << "|";
+		cout << endl;
+	}
+}
+
+void RELATION::reflexive()
+{
+	x=0;
+	y=0;
+	for(i=0; i<nA; i++)
+	{
+		if(RM[i][i] == 1)
+			y++;
+		for(j=0; j<nA; j++)
+		{
+			x+=RM[i][j];
+		}
+	}
+	if(x == nA && y == nA)
+		cout << "\nRelation is REFLEXIVE.";
+	else
+		cout << "\nRelation is NOT REFLEXIVE.";
+}
 
 int main()
 {
 	RELATION ob;
 	
-	ob.inputSets();
+	ob.inputSet();
+	ob.printSet();
 	ob.inputRelation();
-	ob.printSets();
 	ob.printRelation();
+	ob.Matrix();
+	ob.reflexive();
 }
